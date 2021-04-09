c85a908eeadf7052daf85c4f29a2d3411129cd1f
MiaoDaMi <1643317686@qq.com>
2021-04-09 14:19:57 +0800

    init a template

---
+++ app.js
@@ -0,0 +1,23 @@
+// app.js
+//app.js
+let config = require('./pages/js/config.js')
+const app = getApp()
+App({
+  config: config,
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/js/config.js
@@ -0,0 +1,7 @@
+const apiUrl = 'http://www.erguo.ltd:3003/api'
+const imgUrl = ''
+
+module.exports = {
+  apiUrl,
+  imgUrl,
+}
